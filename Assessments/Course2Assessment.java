6cb830a5e31f6d3b1891ae195a26f899bce82ff9
Ruchitha0123 <ruchireddy110@gmail.com>
2025-07-23 12:42:53 +0530

    Add files via upload

---
+++ Assessments/Course2Assessment.java
@@ -0,0 +1,16 @@
+package Assessments;
+import java.util.Random;
+public class Course2Assessment {
+	public static void main(String[] args) {
+		long random10DigitNumber = generateTenDigitRandomNumber();
+		System.out.println("Random 10-Digit Number: " + random10DigitNumber);
+	}
+	public static long generateTenDigitRandomNumber() {
+		Random random = new Random();
+		long min = 1000000000L;
+		long max = 9999999999L;
+		long random10DigitNumber = min + ((long) (random.nextDouble() * (max - min + 1)));
+		return random10DigitNumber;
+	}
+}
+
